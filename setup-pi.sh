1132c8d429956cd1fbe5ecf7849f70f1b39aacc2
Spencer <spencerbriere@gmail.com>
2017-12-28 17:31:36 -0700

    Updated setup-pi

--- setup-pi.sh
+++ setup-pi.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
-if [ "$EUID" -e 0 ]
+if [ "$(whoami)" != "pi" ]
   then echo "Please run as Pi."
   exit
 fi
 echo "1. Change password. You will be prompted to change your password."
 read -p "Press any key to continue... " -n1 -s
+echo ""
 passwd
 
 echo "2. Enable SSH. You will be prompted to enable SSH."
+echo "Steps are: 5 then P2 then <yes> then <finish>"
 read -p "Press any key to continue... " -n1 -s
-
+echo ""
 sudo raspi-config
