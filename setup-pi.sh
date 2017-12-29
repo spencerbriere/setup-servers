619eba4c4542fdee29f19ba0a79d645c3449bd8d
Spencer <spencerbriere@gmail.com>
2017-12-28 17:40:14 -0700

    Script now requires root

--- setup-pi.sh
+++ setup-pi.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
-if [ "$(whoami)" != "pi" ]
-  then echo "Please run as Pi."
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root."
   exit
 fi
+
 echo "1. Change password. You will be prompted to change your password."
 read -p "Press any key to continue... " -n1 -s
 echo ""
-passwd
+sudo -u "pi" passwd
 
 echo "2. Enable SSH. You will be prompted to enable SSH."
-echo "Steps are: 5 then P2 then <yes> then <finish>"
+echo "Steps are: 5 then P2 then <yes>, <ok> then <finish>"
 read -p "Press any key to continue... " -n1 -s
 echo ""
 sudo raspi-config
